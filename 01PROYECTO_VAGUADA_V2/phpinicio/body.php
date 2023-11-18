3185c5ccc4cbad72f707d6955bb5c764f7383d11
SrChedar <chedar444@gmail.com>
2023-11-18 13:59:05 +0100

    Versión rrhh

--- 01PROYECTO_VAGUADA_V2/phpinicio/body.php
+++ 01PROYECTO_VAGUADA_V2/phpinicio/body.php
@@ -4,23 +4,23 @@
     <body>
         <main class="maincontenedor1">
             <div id="separadordelvideo" class="divtituloinsta">
-                <h1 class="tituloinsta" id="novedades">novedades</h1>
+                <h1  id="upp" class="tituloinsta">novedades</h1>
             </div>
-            <div class="titulobox"><a href="/CarpetaRopa/ColeccionMujer/mujer.html">ropa de mujer</a></div>
+            <div class="titulobox"><a id="upp" href="/CarpetaRopa/ColeccionMujer/mujer.html">ropa de mujer</a></div>
             <div class="divboxinicio">
                 <div class="onebox"><a href="/CarpetaRopa/ColeccionMujer/Productos/producto002.html"><img src="./ARCHIVOS/fotos/Ropa/Mujer/Editadas/002.jpg" alt=""><div id="upp"><h3>vestido flores tul</h3><h3>39,50€</h3></div></a></div>
                 <div class="twobox"><a href="/CarpetaRopa/ColeccionMujer/Productos/producto001.html"><img src="./ARCHIVOS/fotos/Ropa/Mujer/Editadas/001.jpg" alt=""><h3>chaqueta traje lisa</h3><h3>69,99€</h3></a></div>
                 <div class="threebox"><a href="/CarpetaRopa/ColeccionMujer/Productos/producto003.html"><img src="./ARCHIVOS/fotos/Ropa/Mujer/Editadas/003.jpg" alt=""><h3>abrigo solapa largo</h3><h3>89,99€</h3></a></div>
                 <div class="fourbox"><a href="/CarpetaRopa/ColeccionMujer/mujer.html"><img src="./ARCHIVOS/fotos/boton-siguiente.png" alt=""></a></div>
             </div>
-            <div class="titulobox"><a href="/CarpetaRopa/ColeccionHombre/hombre.html">ropa de hombre</a></div>
+            <div class="titulobox"><a id="upp" href="/CarpetaRopa/ColeccionHombre/hombre.html">ropa de hombre</a></div>
             <div class="divboxinicio">
                 <div class="onebox"><a href="/CarpetaRopa/ColeccionHombre/Productos/001.html"><img src="./ARCHIVOS/fotos/Ropa/Hombre/Editadas/001.jpg" alt=""><h3>pantalón chino</h3><h3>29,99€</h3></a></div>
                 <div class="twobox"><a href="/CarpetaRopa/ColeccionHombre/Productos/002.html"><img src="./ARCHIVOS/fotos/Ropa/Hombre/Editadas/002.jpg" alt=""><h3>pantalón cargo</h3><h3>29,99€</h3></a></div>
                 <div class="threebox"><a href="/CarpetaRopa/ColeccionHombre/Productos/003.html"><img src="./ARCHIVOS/fotos/Ropa/Hombre/Editadas/003.jpg" alt=""><h3>pantalón recto bolsillos</h3><h3>29,99€</h3></a></div>
                 <div class="fourbox"><a href="/CarpetaRopa/ColeccionHombre/hombre.html"><img src="./ARCHIVOS/fotos/boton-siguiente.png" alt=""></a></div>
             </div>
-            <div class="titulobox"><a href="/CarpetaRopa/ColecciónNiño/niño.html">ropa infantil</a></div>
+            <div class="titulobox"><a id="upp" href="/CarpetaRopa/ColecciónNiño/niño.html">ropa infantil</a></div>
             <div class="divboxinicio">
                 <div class="onebox"><a href="/CarpetaRopa/ColecciónNiño/Productos/producto005.html"><img src="./ARCHIVOS/fotos/Ropa/Niño/foto5/005_editada.jpg" alt=""><h3>chaqueta bordados</h3><h3>16,99€</h3></a></div>
                 <div class="twobox"><a href="/CarpetaRopa/ColecciónNiño/Productos/producto001.html"><img src="./ARCHIVOS/fotos/Ropa/Niño/foto1/001_editada.jpg" alt=""><h3>pack 3 camisetas lisas</h3><h3>14,95€</h3></a></div>
@@ -28,42 +28,11 @@
                 <div class="fourbox"><a href="/CarpetaRopa/ColecciónNiño/niño.html"><img src="./ARCHIVOS/fotos/boton-siguiente.png" alt=""></a></div>
             </div>
             <!--FINAL Contenedor de fotos Menu-->
-            <!--INICIO APARTADO INSTAGRAM-->
-            <div class="fondoinstagram">
-                <div class="tituloinstagram" >
-                    <h1>instagram</h1>
-                </div>
-                <div class="grid-container">
-                    <div class="grid-item">
-                        <a href="https://www.instagram.com/p/Cy6V2flvGGC/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="./ARCHIVOS/fotos/Grid_Insta/001.jpg" alt="Imagen 1"></a>
-                    </div>
-                    <div class="grid-item">
-                        <a href="https://www.instagram.com/p/Cy_mJBiLBZl/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="./ARCHIVOS/fotos/Grid_Insta/002.jpg" alt="Imagen 2"></a>
-                    </div>
-                    <div class="grid-item">
-                        <a href="https://www.instagram.com/p/Cy86kllhCzi/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="./ARCHIVOS/fotos/Grid_Insta/003.jpg" alt="Imagen 3"></a>
-                    </div>
-                    <div class="grid-item">
-                        <a href="https://www.instagram.com/p/CzMeHetsA83/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="./ARCHIVOS/fotos/Grid_Insta/004.jpg" alt="Imagen 4"></a>
-                    </div>
-                    <div class="grid-item">
-                        <a href="https://www.instagram.com/p/CzPC6-1poS_/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="./ARCHIVOS/fotos/Grid_Insta/005.jpg" alt="Imagen 5"></a>
-                    </div>
-                    <div class="grid-item">
-                        <a href="https://www.instagram.com/p/CzRFXkmIDYI/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="./ARCHIVOS/fotos/Grid_Insta/006.jpg" alt="Imagen 6"></a>
-                    </div>
-                    <div class="grid-item">
-                        <a href="https://www.instagram.com/p/CzUMhcQtL_K/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="./ARCHIVOS/fotos/Grid_Insta/007.jpg" alt="Imagen 7"></a>
-                    </div>
-                    <div class="grid-item">
-                        <a href="https://www.instagram.com/p/CzW_BZGpgQl/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="./ARCHIVOS/fotos/Grid_Insta/008.jpg" alt="Imagen 8"></a>
-                    </div>
-                    <div class="grid-item">
-                        <a href="https://www.instagram.com/p/Cyq46QDCswu/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=="><img src="./ARCHIVOS/fotos/Grid_Insta/009.jpg" alt="Imagen 9"></a>
-                    </div>
-                </div>
+            
+            <!--INICIO APARTADO RRSS-->
+            <div class="" >
+                <h1></h1>
             </div>
-            <!--FINAL APARTADO INSTAGRAM-->
-
+            <!--FIN APARTADO RRSS-->
         </main>
     </body>
