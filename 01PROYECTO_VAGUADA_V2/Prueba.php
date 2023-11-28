d058b9f98461f6afaa71c92fe87ca7d782d8c81e
Pablo Giménez Giner <pablogimenezginer@gmail.com>
2023-11-28 13:31:35 +0100

    Prueba Drive completada
    Un éxito rotundo, ahora toca decidir si lo hacemos asi o de otra manera. De momento voy a crear una cuenta para tener un Drive nuevo y compartire la carpeta donde esten las fotos

---
+++ 01PROYECTO_VAGUADA_V2/Prueba.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="http://drive.google.com/uc?export=view&id=18fQgeu7EaN0K71wZP1nXwLf0rjZ37Jmv">
+</body>
+</html>
