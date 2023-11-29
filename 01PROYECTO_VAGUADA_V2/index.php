9f7dfd963b4d34d71c466ac1134fae12b89d01d0
Pablo Giménez Giner <pablogimenezginer@gmail.com>
2023-11-29 12:47:58 +0100

    a
    a

--- 01PROYECTO_VAGUADA_V2/index.php
+++ 01PROYECTO_VAGUADA_V2/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sfera</title>
-    <link rel="icon" type="image/x-icon" src="http://drive.google.com/uc?export=view&id=1rOy5lWGsWbryxJL5eWdvYbyMtVnU1Yd0">
+    <link rel="icon" type="image/x-icon" href="http://drive.google.com/uc?export=view&id=1rOy5lWGsWbryxJL5eWdvYbyMtVnU1Yd0">
     <link rel="stylesheet" type="text/css" href="estilos/estilo2.css">
     <link rel="stylesheet" type="text/css" href="estilos/estilomenufooter.css">
 </head>
